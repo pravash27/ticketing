9aa70b271a50ecada42ad541ee9dd17704184696
pravash27 <pravashranjansharma@gmail.com>
2023-09-30 20:53:48 +0530

    comment added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import { app } from './app'
 const start = async () => {
-    console.log("Auth Started")
+    console.log("Auth Startedff")
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY is not specified");
     }
